d1940d25149b71f923717e09e16b9f0aec717c4a
WATGAMER\snmalton <snmalton@edu.uwaterloo.ca>
2017-07-06 13:42:31 -0400

    Polygons returned will now be sorted by their name

--- server/data-access/functions/polygons/getAll.js
+++ server/data-access/functions/polygons/getAll.js
@@ -2,6 +2,7 @@ const Polygon = rootRequire("server/schemas/polygon");
 
 function GetAll(cb) {
     Polygon.find({})
+        .sort("name")
         .exec((err, polygons) => {
             if (err) {
                 return cb({error: err});

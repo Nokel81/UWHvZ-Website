99ff4f66f2028db65ce4fb01a756a1c942313597
Sebastian Malton <sebastian@malton.name>
2017-07-13 01:33:33 -0400

    Mongoose function is remove not delete

--- server/data-access/functions/report/deleteReport.js
+++ server/data-access/functions/report/deleteReport.js
@@ -2,7 +2,7 @@ const Report = rootRequire("server/schemas/report");
 const findByGame = rootRequire("server/data-access/functions/report/findByGame");
 
 function DeleteReport(reportId, gameId, cb) {
-    Report.delete({_id: reportId})
+    Report.remove({_id: reportId})
         .exec((err, report) => {
             if (err) {
                 return cb({error: err});

e18c5a817f410077fac2af07690a301eb2a9bb74
WATGAMER\snmalton <snmalton@edu.uwaterloo.ca>
2017-07-20 11:38:10 -0400

    Finished promising graphs

--- server/data-access/functions/graphs/getReports.js
+++ server/data-access/functions/graphs/getReports.js
@@ -1,58 +1,53 @@
+const Promise = require('bluebird');
+
 const findCurrentOrNext = rootRequire('server/data-access/functions/game/findCurrentOrNext');
 const Report = rootRequire('server/schemas/report');
 
-function GetReports(userId, cb) {
-    findCurrentOrNext(res => {
-        if (res.error) {
-            return cb(res);
-        }
-        let game = res.body;
+function GetReports(userId) {
+    return new Promise(function(resolve, reject) {
         let graph = [];
-        let isHumanKnowledge = game.humans.indexOf(userId) >= 0 || !userId || userId == "null" || userId == "undefined";
-
         let zombieCount = 0;
         let stunCount = 0;
-        if (!isHumanKnowledge) {
-            zombieCount = game.originalZombies.length;
-        }
-        graph.push({
-            x: game.startDate,
-            y: zombieCount,
-            group: "Zombies"
-        });
-        graph.push({
-            x: game.startDate,
-            y: stunCount,
-            group: "Stuns"
-        });
-
-        Report.find({gameId: game._id, ratified: true})
-            .sort("time")
-            .exec((err, reports) => {
-                if (err) {
-                    return cb({error: err});
-                }
-
-                reports.forEach(report => {
-                    if (report.reportType === "Stun") {
-                        stunCount++;
-                        graph.push({
-                            x: report.time,
-                            y: stunCount,
-                            group: "Stuns"
-                        });
-                    } else {
-                        zombieCount++;
-                        graph.push({
-                            x: report.time,
-                            y: zombieCount,
-                            group: "Zombies"
-                        });
-                    }
-                });
-
-                cb({body: graph});
+        findCurrentOrNext()
+        .then(game => {
+            if (game.humans.indexOf(userId) >= 0 || !userId || userId == "null" || userId == "undefined") {
+                zombieCount = game.originalZombies.length;
+            }
+            graph.push({
+                x: game.startDate,
+                y: zombieCount,
+                group: "Zombies"
             });
+            graph.push({
+                x: game.startDate,
+                y: stunCount,
+                group: "Stuns"
+            });
+            return Report.find({gameId: game._id, ratified: true}).sort("time").exec();
+        })
+        .then(reports => {
+            reports.forEach(report => {
+                if (report.reportType === "Stun") {
+                    stunCount++;
+                    graph.push({
+                        x: report.time,
+                        y: stunCount,
+                        group: "Stuns"
+                    });
+                } else {
+                    zombieCount++;
+                    graph.push({
+                        x: report.time,
+                        y: zombieCount,
+                        group: "Zombies"
+                    });
+                }
+            });
+            resolve(graph);
+        })
+        .catch(error => {
+            reject(error);
+        });
     });
 }
 

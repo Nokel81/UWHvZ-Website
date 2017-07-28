c8ea1ed8123f063db2095f96263bc542ce161001
WATGAMER\snmalton <snmalton@edu.uwaterloo.ca>
2017-07-28 10:56:46 -0400

    Adding checks to make sure that the player fields are the same to the game being searched for (makes them read only)

--- server/data-access/functions/game/update.js
+++ server/data-access/functions/game/update.js
@@ -5,7 +5,15 @@ const findById = rootRequire("server/data-access/functions/game/findById");
 
 function Update(game) {
     return new Promise(function(resolve, reject) {
-        Game.updateOne({_id: game._id}, game)
+        Game.updateOne({
+            _id: game._id,
+            // The next fields to make these fields ReadOnly
+            zombies: game.zombies,
+            originalZombies: game.originalZombies,
+            humans: game.humans,
+            moderators: game.moderators,
+            spectators: game.spectators,
+        }, game)
         .exec()
         .then(game => {
             return findById(game._id);

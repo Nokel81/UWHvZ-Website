86d2bb1724010d00f1f5c1c1649d865f70ad4e53
Sebastian Malton <sebastian@malton.name>
2017-11-01 14:44:11 -0400

    game name

--- server/data-access/functions/graphs/getAllTrees.js
+++ server/data-access/functions/graphs/getAllTrees.js
@@ -105,6 +105,7 @@ function GetAllTrees(userType, isSuper) {
                                         }
                                     });
                                 });
+                                console.log(game.name);
                                 console.log(taggersWhoAreNotTagged);
                                 taggersWhoAreNotTagged.forEach(zom => {
                                     edges.push({
